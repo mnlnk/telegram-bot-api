9b71c89c004654a212745a52d9c2a86cea984ca1
mnlnk <u.mnlnk@gmail.com>
2024-04-04 02:59:58 +0500

    Добавляет класс сущности Birthdate

---
+++ src/Entities/Birthdate.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Manuylenko\Telegram\Bot\Api\Entities;
+
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
+
+/**
+ * Представляет день рождения пользователя.
+ *
+ * @link https://core.telegram.org/bots/api#birthdate
+ *
+ * @method int getDay()       День.
+ * @method int getMonth()     Месяц.
+ * @method int getYear()  (+) Год.
+ */
+#[Required([
+    'day',
+    'month'
+])]
+class Birthdate extends Entity
+{
+    //
+}
