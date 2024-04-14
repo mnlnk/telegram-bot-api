751bd31537ac0f95aa7d774a63f049ca77306937
mnlnk <u.mnlnk@gmail.com>
2024-04-15 00:05:49 +0500

    API: Добавляет параметр $businessConnectionId в метод sendDice()

--- src/Api.php
+++ src/Api.php
@@ -972,7 +972,8 @@ class Api
         ?bool $protectContent = null,
         ?int $messageThreadId = null,
         ?ReplyParameters $replyParameters = null,
-        ?KeyboardMarkup $replyMarkup = null
+        ?KeyboardMarkup $replyMarkup = null,
+        ?string $businessConnectionId = null
     ): Message
     {
         return EntityFactory::make(Message::class, $this->call(func_get_args(), [
