b52aa7dc0fab707190402898ca52c119a3fcb675
mnlnk <u.mnlnk@gmail.com>
2024-04-15 02:12:46 +0500

    KeyboardButtonRequestUsers: Добавляет методы для работы с полями "request_name", "request_username" и "request_photo"

--- src/Entities/Keyboards/Buttons/Actions/KeyboardButtonRequestUsers.php
+++ src/Entities/Keyboards/Buttons/Actions/KeyboardButtonRequestUsers.php
@@ -14,15 +14,21 @@ use Manuylenko\Telegram\Bot\Api\Entities\Keyboards\Buttons\Button;
  *
  * @link https://core.telegram.org/bots/api#keyboardbuttonrequestusers
  *
- * @method       int getRequestId()         32-битный (signed) идентификатор запроса.
- * @method bool|null getUserIsBot()     (+) Запрашиваемые пользователи являются ботами.
- * @method bool|null getUserIsPremium() (+) Запрашиваемые пользователи являются пользователями Телеграм Премиум.
- * @method  int|null getMaxQuantity()   (+) Максимальное количество пользователей, которые будут выбраны.
+ * @method       int getRequestId()           32-битный (signed) идентификатор запроса.
+ * @method bool|null getUserIsBot()       (+) Запрашивать ботов.
+ * @method bool|null getUserIsPremium()   (+) Запрашивать пользователей Телеграм Премиум.
+ * @method bool|null getRequestName()     (+) Запрашивать имена и фамилии пользователей.
+ * @method bool|null getRequestUsername() (+) Запрашивать юзернеймы пользователей.
+ * @method bool|null getRequestPhoto()    (+) Запрашивать фото пользователей.
+ * @method  int|null getMaxQuantity()     (+) Максимальное количество пользователей, которые будут выбраны.
  *
- * @method $this setRequestId(int $requestId)          32-битный (signed) идентификатор запроса.
- * @method $this setUserIsBot(bool $userIsBot)         Запрашиваемые пользователи являются ботами.
- * @method $this setUserIsPremium(bool $userIsPremium) Запрашиваемые пользователи являются пользователями Телеграм Премиум.
- * @method $this setMaxQuantity(int $maxQuantity)      Максимальное количество пользователей, которые будут выбраны.
+ * @method $this setRequestId(int $requestId)              32-битный (signed) идентификатор запроса.
+ * @method $this setUserIsBot(bool $userIsBot)             Запрашивать ботов.
+ * @method $this setUserIsPremium(bool $userIsPremium)     Запрашивать пользователей Телеграм Премиум.
+ * @method $this setRequestName(bool $requestName)         Запрашивать имена и фамилии пользователей.
+ * @method $this setRequestUsername(bool $requestUsername) Запрашивать юзернеймы пользователей.
+ * @method $this setRequestPhoto(bool $requestPhoto)       Запрашивать фото пользователей.
+ * @method $this setMaxQuantity(int $maxQuantity)          Максимальное количество пользователей, которые будут выбраны.
  */
 #[Required([
     'request_id'
@@ -36,7 +42,10 @@ class KeyboardButtonRequestUsers extends Button
         int $requestId,
         ?bool $userIsBot = null,
         ?bool $userIsPremium = null,
-        ?int $maxQuantity = null // 1-10
+        ?bool $requestName = null,
+        ?bool $requestUsername = null,
+        ?bool $requestPhoto = null,
+        ?int $maxQuantity = null, // 1-10
     ): static
     {
         return static::fromArgs(func_get_args());
