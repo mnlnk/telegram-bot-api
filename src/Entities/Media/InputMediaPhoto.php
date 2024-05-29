614b18fcbb480e25e8c502cb0cfd9febf87cd5f1
mnlnk <u.mnlnk@gmail.com>
2024-05-30 01:52:58 +0500

    InputMediaPhoto: Добавляет методы для работы c параметром "show_caption_above_media"

--- src/Entities/Media/InputMediaPhoto.php
+++ src/Entities/Media/InputMediaPhoto.php
@@ -12,18 +12,20 @@ use Manuylenko\Telegram\Bot\Api\Entities\Messages\MessageEntity;
  *
  * @link https://core.telegram.org/bots/api#inputmediaphoto
  *
- * @method               string getType()                Тип результата.
- * @method               string getMedia()               Медиа-файл.
- * @method          string|null getCaption()         (+) Подпись файла.
- * @method          string|null getParseMode()       (+) Режим разбора специальных сущностей в подписи.
- * @method MessageEntity[]|null getCaptionEntities() (+) Массив объектов специальных сущностей, появляющихся в подписи.
- * @method            bool|null getHasSpoiler()      (+) Закрыто анимацией спойлера.
+ * @method               string getType()                      Тип результата.
+ * @method               string getMedia()                     Медиа-файл.
+ * @method          string|null getCaption()               (+) Подпись файла.
+ * @method          string|null getParseMode()             (+) Режим разбора специальных сущностей в подписи.
+ * @method MessageEntity[]|null getCaptionEntities()       (+) Массив объектов специальных сущностей, появляющихся в подписи.
+ * @method            bool|null getShowCaptionAboveMedia() (+) Показывать подпись над медиа.
+ * @method            bool|null getHasSpoiler()            (+) Закрыто анимацией спойлера.
  *
- * @method $this setMedia(string $media)                              Медиа-файл.
- * @method $this setCaption(string $caption)                          Подпись файла.
- * @method $this setParseMode(string $parseMode)                      Режим разбора специальных сущностей в подписи.
- * @method $this setCaptionEntities(MessageEntity[] $captionEntities) Массив объектов специальных сущностей, появляющихся в подписи.
- * @method $this setHasSpoiler(bool $hasSpoiler)                      Закрыто анимацией спойлера.
+ * @method $this setMedia(string $media)                               Медиа-файл.
+ * @method $this setCaption(string $caption)                           Подпись файла.
+ * @method $this setParseMode(string $parseMode)                       Режим разбора специальных сущностей в подписи.
+ * @method $this setCaptionEntities(MessageEntity[] $captionEntities)  Массив объектов специальных сущностей, появляющихся в подписи.
+ * @method $this setShowCaptionAboveMedia(bool $showCaptionAboveMedia) Показывать подпись над медиа.
+ * @method $this setHasSpoiler(bool $hasSpoiler)                       Закрывать анимацией спойлера.
  */
 #[Required([
     'type',
@@ -56,6 +58,7 @@ class InputMediaPhoto extends InputMedia
         ?string $caption = null,
         ?string $parseMode = null,
         ?array $captionEntities = null,
+        ?bool $showCaptionAboveMedia = null,
         ?bool $hasSpoiler = null
     ): static
     {
