5303abd37b46bbdf54ef7c7bb1efc76df801a8bb
mnlnk <u.mnlnk@gmail.com>
2024-05-30 01:54:11 +0500

    InputMediaVideo: Добавляет методы для работы c параметром "show_caption_above_media"

--- src/Entities/Media/InputMediaVideo.php
+++ src/Entities/Media/InputMediaVideo.php
@@ -13,28 +13,30 @@ use Manuylenko\Telegram\Bot\Api\Entities\Messages\MessageEntity;
  *
  * @link https://core.telegram.org/bots/api#inputmediavideo
  *
- * @method                string getType()                  Тип результата.
- * @method                string getMedia()                 Медиа-файл.
- * @method InputFile|string|null getThumbnail()         (+) Миниатюра.
- * @method           string|null getCaption()           (+) Подпись файла.
- * @method           string|null getParseMode()         (+) Режим разбора специальных сущностей в подписи.
- * @method  MessageEntity[]|null getCaptionEntities()   (+) Массив объектов специальных сущностей, появляющихся в подписи.
- * @method              int|null getWidth()             (+) Ширина видео.
- * @method              int|null getHeight()            (+) Высота видео.
- * @method              int|null getDuration()          (+) Продолжительность видео в секундах.
- * @method             bool|null getSupportsStreaming() (+) Подходит для потоковой передачи.
- * @method             bool|null getHasSpoiler()        (+) Закрыто анимацией спойлера.
+ * @method                string getType()                      Тип результата.
+ * @method                string getMedia()                     Медиа-файл.
+ * @method InputFile|string|null getThumbnail()             (+) Миниатюра.
+ * @method           string|null getCaption()               (+) Подпись файла.
+ * @method           string|null getParseMode()             (+) Режим разбора специальных сущностей в подписи.
+ * @method  MessageEntity[]|null getCaptionEntities()       (+) Массив объектов специальных сущностей, появляющихся в подписи.
+ * @method             bool|null getShowCaptionAboveMedia() (+) Показывать подпись над медиа.
+ * @method              int|null getWidth()                 (+) Ширина видео.
+ * @method              int|null getHeight()                (+) Высота видео.
+ * @method              int|null getDuration()              (+) Продолжительность видео в секундах.
+ * @method             bool|null getSupportsStreaming()     (+) Подходит для потоковой передачи.
+ * @method             bool|null getHasSpoiler()            (+) Закрыто анимацией спойлера.
  *
- * @method $this setMedia(string $media)                              Медиа-файл.
- * @method $this setThumbnail(InputFile|string $thumbnail)            Миниатюра.
- * @method $this setCaption(string $caption)                          Подпись файла.
- * @method $this setParseMode(string $parseMode)                      Режим разбора специальных сущностей в подписи.
- * @method $this setCaptionEntities(MessageEntity[] $captionEntities) Массив объектов специальных сущностей, появляющихся в подписи.
- * @method $this setWidth(int $width)                                 Ширина видео.
- * @method $this setHeight(int $height)                               Высота видео.
- * @method $this setDuration(int $duration)                           Продолжительность видео в секундах.
- * @method $this setSupportsStreaming(bool $supportsStreaming)        Подходит для потоковой передачи.
- * @method $this setHasSpoiler(bool $hasSpoiler)                      Закрыто анимацией спойлера.
+ * @method $this setMedia(string $media)                               Медиа-файл.
+ * @method $this setThumbnail(InputFile|string $thumbnail)             Миниатюра.
+ * @method $this setCaption(string $caption)                           Подпись файла.
+ * @method $this setParseMode(string $parseMode)                       Режим разбора специальных сущностей в подписи.
+ * @method $this setCaptionEntities(MessageEntity[] $captionEntities)  Массив объектов специальных сущностей, появляющихся в подписи.
+ * @method $this setShowCaptionAboveMedia(bool $showCaptionAboveMedia) Показывать подпись над медиа.
+ * @method $this setWidth(int $width)                                  Ширина видео.
+ * @method $this setHeight(int $height)                                Высота видео.
+ * @method $this setDuration(int $duration)                            Продолжительность видео в секундах.
+ * @method $this setSupportsStreaming(bool $supportsStreaming)         Подходит для потоковой передачи.
+ * @method $this setHasSpoiler(bool $hasSpoiler)                       Закрывать анимацией спойлера.
  */
 #[Required([
     'type',
@@ -68,6 +70,7 @@ class InputMediaVideo extends InputMedia
         ?string $caption = null,
         ?string $parseMode = null,
         ?array $captionEntities = null,
+        ?bool $showCaptionAboveMedia = null,
         ?int $width = null,
         ?int $height = null,
         ?int $duration = null,
