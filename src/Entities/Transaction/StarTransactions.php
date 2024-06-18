4308a0b7c130513c945f959548142247333f68ba
mnlnk <u.mnlnk@gmail.com>
2024-06-19 02:30:37 +0500

    Добавляет класс StarTransactions

---
+++ src/Entities/Transaction/StarTransactions.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Manuylenko\Telegram\Bot\Api\Entities\Transaction;
+
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Depends;
+use Manuylenko\Telegram\Bot\Api\Entities\Entity;
+
+/**
+ * Представляет список транзакций Telegram Star.
+ *
+ * @link https://core.telegram.org/bots/api#startransactions
+ *
+ * @method StarTransaction[] getTransactions() Массив объектов c информацией о транзакциях.
+ */
+#[Depends([
+     'transactions' => [StarTransaction::class]
+])]
+class StarTransactions extends Entity
+{
+    //
+}
