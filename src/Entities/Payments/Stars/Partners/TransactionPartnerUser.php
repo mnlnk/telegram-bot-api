048bd175c80808c594f5d3a40993125a32284f8e
mnlnk <u.mnlnk@gmail.com>
2024-08-15 00:25:24 +0500

    TransactionPartnerUser: Добавляет метод getPaidMedia()

--- src/Entities/Payments/Stars/Partners/TransactionPartnerUser.php
+++ src/Entities/Payments/Stars/Partners/TransactionPartnerUser.php
@@ -5,6 +5,7 @@ namespace Manuylenko\Telegram\Bot\Api\Entities\Payments\Stars\Partners;
 
 use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Depends;
 use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
+use Manuylenko\Telegram\Bot\Api\Entities\Paid\Media\PaidMedia;
 use Manuylenko\Telegram\Bot\Api\Entities\User;
 
 /**
@@ -12,16 +13,18 @@ use Manuylenko\Telegram\Bot\Api\Entities\User;
  *
  * @link https://core.telegram.org/bots/api#transactionpartneruser
  *
- * @method      string getType()               Тип партнера по транзакции.
- * @method        User getUser()               Объект с информацией о пользователе.
- * @method string|null getInvoicePayload() (+) Полезная нагрузка счет-фактуры, указанная ботом.
+ * @method         string getType()               Тип партнера по транзакции.
+ * @method           User getUser()               Объект с информацией о пользователе.
+ * @method    string|null getInvoicePayload() (+) Полезная нагрузка счет-фактуры, указанная ботом.
+ * @method PaidMedia|null getPaidMedia()      (+) Массив объектов с информацией о платных медиа, купленных пользователем.
  */
 #[Required([
     'type',
     'user'
 ])]
 #[Depends([
-    'user' => User::class
+    'user' => User::class,
+    'paid_media' => [PaidMedia::class]
 ])]
 class TransactionPartnerUser extends TransactionPartner
 {
