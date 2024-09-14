aa3d9fa57f349f5e82edb5d8c093d99156c1ab4c
mnlnk <u.mnlnk@gmail.com>
2024-09-14 13:51:35 +0500

    GiveawayCreated: Добавляет метод getPrizeStarCount()

--- src/Entities/Messages/Giveaway/GiveawayCreated.php
+++ src/Entities/Messages/Giveaway/GiveawayCreated.php
@@ -9,6 +9,8 @@ use Manuylenko\Telegram\Bot\Api\Entities\Entity;
  * Представляет служебное сообщение о создании запланированного розыгрыша.
  *
  * @link https://core.telegram.org/bots/api#giveawaycreated
+ *
+ * @method int|null getPrizeStarCount() (+) Количество Telegram Stars, которое будет разделено между победителями розыгрыша.
  */
 class GiveawayCreated extends Entity
 {
