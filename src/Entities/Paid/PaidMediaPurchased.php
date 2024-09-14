4b278da46fcc3ae1a319486636123cf993314939
mnlnk <u.mnlnk@gmail.com>
2024-09-14 12:28:27 +0500

    Добавляет класс сущности PaidMediaPurchased

---
+++ src/Entities/Paid/PaidMediaPurchased.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Manuylenko\Telegram\Bot\Api\Entities\Paid;
+
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Depends;
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
+use Manuylenko\Telegram\Bot\Api\Entities\Entity;
+use Manuylenko\Telegram\Bot\Api\Entities\User;
+
+/**
+ * Представляет информацию о платной покупке медиа.
+ *
+ * @link https://core.telegram.org/bots/api#paidmediapurchased
+ *
+ * @method   User getFrom()             Объект пользователя, купившего медиа.
+ * @method string getPaidMediaPayload() Полезная нагрузка платного медиа, указанная ботом.
+ */
+#[Required([
+    'from',
+    'paid_media_payload'
+])]
+#[Depends([
+    'from' => User::class
+])]
+class PaidMediaPurchased extends Entity
+{
+    //
+}
