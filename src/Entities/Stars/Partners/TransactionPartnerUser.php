55bd57babb63b5e418125a57d083c30fdb7a8c7a
mnlnk <u.mnlnk@gmail.com>
2025-01-02 23:46:01 +0500

    TransactionPartnerUser: Добавляет метод getGift()

--- src/Entities/Stars/Partners/TransactionPartnerUser.php
+++ src/Entities/Stars/Partners/TransactionPartnerUser.php
@@ -5,6 +5,7 @@ namespace Manuylenko\Telegram\Bot\Api\Entities\Stars\Partners;
 
 use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Depends;
 use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
+use Manuylenko\Telegram\Bot\Api\Entities\Messages\Gift;
 use Manuylenko\Telegram\Bot\Api\Entities\Paid\Media\PaidMedia;
 use Manuylenko\Telegram\Bot\Api\Entities\User;
 
@@ -19,6 +20,7 @@ use Manuylenko\Telegram\Bot\Api\Entities\User;
  * @method       int|null getSubscriptionPeriod() (+) Продолжительность платной подписки.
  * @method PaidMedia|null getPaidMedia()          (+) Массив объектов с информацией о платных медиа, купленных пользователем.
  * @method    string|null getPaidMediaPayload()   (+) Полезная нагрузка платного медиа, указанная ботом.
+ * @method      Gift|null getGift()               (+) Объект подарка, отправленного пользователю ботом.
  */
 #[Required([
     'type',
@@ -26,7 +28,8 @@ use Manuylenko\Telegram\Bot\Api\Entities\User;
 ])]
 #[Depends([
     'user' => User::class,
-    'paid_media' => [PaidMedia::class]
+    'paid_media' => [PaidMedia::class],
+    'gift' => Gift::class
 ])]
 class TransactionPartnerUser extends TransactionPartner
 {
