3e02fc8b1abcc6ddf539c0a77b646d6b6cfc6c4b
mnlnk <u.mnlnk@gmail.com>
2025-01-02 16:27:30 +0500

    Добавляет класс сущности TransactionPartnerTelegramApi

---
+++ src/Entities/Payments/Stars/Partners/TransactionPartnerTelegramApi.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace Manuylenko\Telegram\Bot\Api\Entities\Payments\Stars\Partners;
+
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
+
+/**
+ * Представляет транзакцию с оплатой платного вещания.
+ *
+ * @link https://core.telegram.org/bots/api#transactionpartnertelegramapi
+ *
+ * @method string getType()         Тип партнера по транзакции.
+ * @method    int getRequestCount() Количество успешных запросов, которые превысили обычные лимиты и поэтому были выставлены счета.
+ */
+#[Required([
+    'type',
+    'request_count'
+])]
+class TransactionPartnerTelegramApi extends TransactionPartner
+{
+    /**
+     * @inheritDoc
+     */
+    public function __construct(array $data = [])
+    {
+        $data['type'] = TransactionPartnerType::TELEGRAM_API;
+
+        parent::__construct($data);
+    }
+}
