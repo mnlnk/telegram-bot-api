e7db2dd11bcde3205b1ff104a0b470967a5dc785
mnlnk <u.mnlnk@gmail.com>
2025-01-03 01:06:11 +0500

    StarTransaction: Добавляет метод getNanostarAmount()

--- src/Entities/Stars/StarTransaction.php
+++ src/Entities/Stars/StarTransaction.php
@@ -12,11 +12,12 @@ use Manuylenko\Telegram\Bot\Api\Entities\Stars\Partners\TransactionPartner;
  *
  * @link https://core.telegram.org/bots/api#startransaction
  *
- * @method                  string getTransactionId()     Уникальный идентификатор транзакции.
- * @method                     int getAmount()            Количество Telegram Stars, переданных в результате транзакции.
- * @method                     int getDate()              Дата создания транзакции по времени Unix.
- * @method TransactionPartner|null getSource()        (+) Объект источника входящей транзакции (например, покупка пользователем товаров или услуг, Fragment возвращающий деньги при неудачном выводе средств). Только для входящих транзакций.
- * @method TransactionPartner|null getReceiver()      (+) Объект получателя исходящей транзакции (например, пользователь для возврата покупки, Fragment для вывода). Только для исходящих транзакций.
+ * @method                  string getTransactionId()      Уникальный идентификатор транзакции.
+ * @method                     int getAmount()             Количество Telegram Stars, переданных в результате транзакции.
+ * @method                int|null getNanostarAmount() (+) Количество 1/1000000000 акций Telegram Stars, переданных в результате сделки; от 0 до 999999999
+ * @method                     int getDate()               Дата создания транзакции по времени Unix.
+ * @method TransactionPartner|null getSource()         (+) Объект источника входящей транзакции (например, покупка пользователем товаров или услуг, Fragment возвращающий деньги при неудачном выводе средств). Только для входящих транзакций.
+ * @method TransactionPartner|null getReceiver()       (+) Объект получателя исходящей транзакции (например, пользователь для возврата покупки, Fragment для вывода). Только для исходящих транзакций.
  */
 #[Depends([
     'source' => TransactionPartner::class,
