d9a28ccedd49ac5af0739f1447cdea466bb15aed
mnlnk <u.mnlnk@gmail.com>
2025-07-16 13:36:38 +0500

    Video: Добавляет метод getCover()

--- src/Entities/Messages/Video.php
+++ src/Entities/Messages/Video.php
@@ -13,15 +13,16 @@ use Manuylenko\Telegram\Bot\Api\Entities\PhotoSize;
  *
  * @link https://core.telegram.org/bots/api#video
  *
- * @method         string getFileId()           Идентификатор файла.
- * @method         string getFileUniqueId()     Уникальный идентификатор файла.
- * @method            int getWidth()            Ширина видео, определяемая отправителем
- * @method            int getHeight()           Высота видео, определяемая отправителем.
- * @method            int getDuration()         Продолжительность видео в секундах, определяемая отправителем.
- * @method PhotoSize|null getThumbnail()    (+) Объект миниатюры (превью) видео.
- * @method    string|null getFileName()     (+) Оригинальное имя файла, определяемое отправителем.
- * @method    string|null getMimeType()     (+) MIME-тип файла, определяемый отправителем.
- * @method       int|null getFileSize()     (+) Размер файла в байтах.
+ * @method           string getFileId()           Идентификатор файла.
+ * @method           string getFileUniqueId()     Уникальный идентификатор файла.
+ * @method              int getWidth()            Ширина видео, определяемая отправителем
+ * @method              int getHeight()           Высота видео, определяемая отправителем.
+ * @method              int getDuration()         Продолжительность видео в секундах, определяемая отправителем.
+ * @method   PhotoSize|null getThumbnail()    (+) Объект миниатюры (превью) видео.
+ * @method PhotoSize[]|null getCover()        (+) Массив объектов доступных размеров обложки видео.
+ * @method      string|null getFileName()     (+) Оригинальное имя файла, определяемое отправителем.
+ * @method      string|null getMimeType()     (+) MIME-тип файла, определяемый отправителем.
+ * @method         int|null getFileSize()     (+) Размер файла в байтах.
  */
 #[Required([
     'file_id',
@@ -31,7 +32,8 @@ use Manuylenko\Telegram\Bot\Api\Entities\PhotoSize;
     'duration'
 ])]
 #[Depends([
-    'thumbnail' => PhotoSize::class
+    'thumbnail' => PhotoSize::class,
+    'cover' => [PhotoSize::class]
 ])]
 class Video extends Entity
 {
