9ce0b215e712744a5710c28592a09aed03d7af29
mnlnk <u.mnlnk@gmail.com>
2025-07-16 13:38:29 +0500

    Video: Добавляет метод getStartTimestamp()

--- src/Entities/Messages/Video.php
+++ src/Entities/Messages/Video.php
@@ -13,16 +13,17 @@ use Manuylenko\Telegram\Bot\Api\Entities\PhotoSize;
  *
  * @link https://core.telegram.org/bots/api#video
  *
- * @method           string getFileId()           Идентификатор файла.
- * @method           string getFileUniqueId()     Уникальный идентификатор файла.
- * @method              int getWidth()            Ширина видео, определяемая отправителем
- * @method              int getHeight()           Высота видео, определяемая отправителем.
- * @method              int getDuration()         Продолжительность видео в секундах, определяемая отправителем.
- * @method   PhotoSize|null getThumbnail()    (+) Объект миниатюры (превью) видео.
- * @method PhotoSize[]|null getCover()        (+) Массив объектов доступных размеров обложки видео.
- * @method      string|null getFileName()     (+) Оригинальное имя файла, определяемое отправителем.
- * @method      string|null getMimeType()     (+) MIME-тип файла, определяемый отправителем.
- * @method         int|null getFileSize()     (+) Размер файла в байтах.
+ * @method           string getFileId()             Идентификатор файла.
+ * @method           string getFileUniqueId()       Уникальный идентификатор файла.
+ * @method              int getWidth()              Ширина видео, определяемая отправителем
+ * @method              int getHeight()             Высота видео, определяемая отправителем.
+ * @method              int getDuration()           Продолжительность видео в секундах, определяемая отправителем.
+ * @method   PhotoSize|null getThumbnail()      (+) Объект миниатюры (превью) видео.
+ * @method PhotoSize[]|null getCover()          (+) Массив объектов доступных размеров обложки видео.
+ * @method         int|null getStartTimestamp() (+) Временная метка в секундах, с которой начнется воспроизведение видео.
+ * @method      string|null getFileName()       (+) Оригинальное имя файла, определяемое отправителем.
+ * @method      string|null getMimeType()       (+) MIME-тип файла, определяемый отправителем.
+ * @method         int|null getFileSize()       (+) Размер файла в байтах.
  */
 #[Required([
     'file_id',
