c343ac7483dce3a42f064f1ad0e91de547ae0e74
mnlnk <u.mnlnk@gmail.com>
2025-07-19 01:39:46 +0500

    StoryAreaPosition: Добавляет методы для установки параметров сущности

--- src/Entities/Stories/StoryAreaPosition.php
+++ src/Entities/Stories/StoryAreaPosition.php
@@ -16,8 +16,28 @@ use Manuylenko\Telegram\Bot\Api\Entities\Entity;
  * @method float getHeightPercentage()       Высота прямоугольника области в процентах от высоты медиа.
  * @method float getRotationAngle()          Угол поворота прямоугольника по часовой стрелке, в градусах; 0-360.
  * @method float getCornerRadiusPercentage() Радиус скругления угла прямоугольника в процентах от ширины медиа.
+ *
+ * @method $this setXPercentage(float $xPercentage)                       Абсцисса центра области, в процентах от ширины медиа.
+ * @method $this setYPercentage(float $yPercentage)                       Ордината центра области, в процентах от высоты медиа.
+ * @method $this setWidthPercentage(float $widthPercentage)               Ширина прямоугольника области в процентах от ширины медиа.
+ * @method $this setHeightPercentage(float $heightPercentage)             Высота прямоугольника области в процентах от высоты медиа.
+ * @method $this setRotationAngle(float $rotationAngle)                   Угол поворота прямоугольника по часовой стрелке, в градусах; 0-360.
+ * @method $this setCornerRadiusPercentage(float $cornerRadiusPercentage) Радиус скругления угла прямоугольника в процентах от ширины медиа.
  */
 class StoryAreaPosition extends Entity
 {
-    //
+    /**
+     * Создает объект сущности.
+     */
+    public static function make(
+        float $xPercentage,
+        float $yPercentage,
+        float $widthPercentage,
+        float $heightPercentage,
+        float $rotationAngle,
+        float $cornerRadiusPercentage
+    ): static
+    {
+        return static::fromArgs(func_get_args());
+    }
 }
