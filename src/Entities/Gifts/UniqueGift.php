3513f359276c73e1ff10d4f9e420b816ae81ecf1
mnlnk <u.mnlnk@gmail.com>
2025-07-18 22:38:15 +0500

    Добавляет класс сущности UniqueGift

---
+++ src/Entities/Gifts/UniqueGift.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Manuylenko\Telegram\Bot\Api\Entities\Gifts;
+
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Depends;
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
+use Manuylenko\Telegram\Bot\Api\Entities\Entity;
+
+/**
+ * Представляет уникальный подарок, представляющий собой улучшенную версию обычного подарка.
+ *
+ * @link https://core.telegram.org/bots/api#uniquegift
+ *
+ * @method             string getBaseName() Человекочитаемое название обычного подарка, из которого был улучшен этот уникальный подарок.
+ * @method             string getName()     Уникальное название подарка. Его можно использовать в ссылках https://t.me/nft/... и разделах с историями.
+ * @method                int getNumber()   Уникальный номер улучшенного подарка среди подарков, улучшенных с одного и того же обычного подарка.
+ * @method    UniqueGiftModel getModel()    Объект модели подарка
+ * @method   UniqueGiftSymbol getSymbol()   Объект символа подарка.
+ * @method UniqueGiftBackdrop getBackdrop() Объект задника подарка.
+ */
+#[Required([
+    'base_name',
+    'name',
+    'number',
+    'model',
+    'symbol',
+    'backdrop'
+])]
+#[Depends([
+    'model' => UniqueGiftModel::class,
+    'symbol' => UniqueGiftSymbol::class,
+    'backdrop' => UniqueGiftBackdrop::class
+])]
+class UniqueGift extends Entity
+{
+    //
+}
