1c6efaad5d7ad5adab824507810cf77b19084ead
mnlnk <u.mnlnk@gmail.com>
2025-07-18 22:33:55 +0500

    UniqueGiftBackdrop: Исправляет описание

--- src/Entities/Gifts/UniqueGiftBackdrop.php
+++ src/Entities/Gifts/UniqueGiftBackdrop.php
@@ -8,13 +8,13 @@ use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
 use Manuylenko\Telegram\Bot\Api\Entities\Entity;
 
 /**
- * Описывает предысторию уникального подарка.
+ * Представляет задник уникального подарка.
  *
  * @link https://core.telegram.org/bots/api#uniquegiftbackdrop
  *
- * @method                   string getName()           Название фона.
- * @method UniqueGiftBackdropColors getColors()         Объект с описанием цветов фона.
- * @method                      int getRarityPerMille() Количество уникальных подарков, которые получают этот фон, на каждые 1000 улучшенных подарков.
+ * @method                   string getName()           Название задника.
+ * @method UniqueGiftBackdropColors getColors()         Объект с описанием цветов задник.
+ * @method                      int getRarityPerMille() Количество уникальных подарков, которые получают этот задник, на каждые 1000 улучшенных подарков.
  */
 #[Required([
     'name',
