ff9751de2ed97408a5b3d3dfaeab91a2ebe73544
mnlnk <u.mnlnk@gmail.com>
2025-07-18 22:34:43 +0500

    UniqueGiftBackdropColors: Исправляет описание

--- src/Entities/Gifts/UniqueGiftBackdropColors.php
+++ src/Entities/Gifts/UniqueGiftBackdropColors.php
@@ -7,14 +7,14 @@ use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
 use Manuylenko\Telegram\Bot\Api\Entities\Entity;
 
 /**
- * Описывает цвета фона уникального подарка.
+ * Описывает цвета задника уникального подарка.
  *
  * @link https://core.telegram.org/bots/api#uniquegiftbackdropcolors
  *
- * @method int getCenterColor() Цвет в центре фона в формате RGB.
- * @method int getEdgeColor()   Цвет по краям фона в формате RGB.
+ * @method int getCenterColor() Цвет в центре задника в формате RGB.
+ * @method int getEdgeColor()   Цвет по краям задника в формате RGB.
  * @method int getSymbolColor() Цвет, который будет применен к символу в формате RGB.
- * @method int getTextColor()   Цвет текста на фоне в формате RGB.
+ * @method int getTextColor()   Цвет текста на заднике в формате RGB.
  */
 #[Required([
     'center_color',
