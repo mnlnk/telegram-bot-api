1cdedd24b13b48048ca2246bb2610118a9b8cfc1
mnlnk <u.mnlnk@gmail.com>
2025-07-18 22:05:23 +0500

    Добавляет класс сущности UniqueGiftBackdropColors

---
+++ src/Entities/Gifts/UniqueGiftBackdropColors.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Manuylenko\Telegram\Bot\Api\Entities\Gifts;
+
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
+use Manuylenko\Telegram\Bot\Api\Entities\Entity;
+
+/**
+ * Описывает цвета фона уникального подарка.
+ *
+ * @link https://core.telegram.org/bots/api#uniquegiftbackdropcolors
+ *
+ * @method int getCenterColor() Цвет в центре фона в формате RGB.
+ * @method int getEdgeColor()   Цвет по краям фона в формате RGB.
+ * @method int getSymbolColor() Цвет, который будет применен к символу в формате RGB.
+ * @method int getTextColor()   Цвет текста на фоне в формате RGB.
+ */
+#[Required([
+    'center_color',
+    'edge_color',
+    'symbol_color',
+    'text_color'
+])]
+class UniqueGiftBackdropColors extends Entity
+{
+    //
+}
