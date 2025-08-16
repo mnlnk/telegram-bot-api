dbff34f6526dd48b098d87f56626181cbca6a060
mnlnk <u.mnlnk@gmail.com>
2025-08-16 12:59:34 +0500

    API: Добавляет параметр $suggestedPostParameters в метод sendAnimation()

--- src/Api.php
+++ src/Api.php
@@ -945,6 +945,7 @@ class Api
         ?bool $protectContent = null,
         ?bool $allowPaidBroadcast = null,
         ?string $messageEffectId = null,
+        ?SuggestedPostParameters $suggestedPostParameters = null,
         ?int $messageThreadId = null,
         ?int $directMessagesTopicId = null,
         ?ReplyParameters $replyParameters = null,
@@ -954,6 +955,7 @@ class Api
     {
         return EntityFactory::make(Message::class, $this->call(func_get_args(), [
             'caption_entities',
+            'suggested_post_parameters',
             'reply_parameters',
             'reply_markup'
         ]));
