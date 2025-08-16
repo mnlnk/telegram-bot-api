eecbf5881b15d6c8605a5bdbde6a5f3a892b4628
mnlnk <u.mnlnk@gmail.com>
2025-08-16 11:21:14 +0500

    Gift: Добавляет метод getPublisherChat()

--- src/Entities/Gifts/Gift.php
+++ src/Entities/Gifts/Gift.php
@@ -5,6 +5,7 @@ namespace Manuylenko\Telegram\Bot\Api\Entities\Gifts;
 
 use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Depends;
 use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
+use Manuylenko\Telegram\Bot\Api\Entities\Chat\Chat;
 use Manuylenko\Telegram\Bot\Api\Entities\Entity;
 use Manuylenko\Telegram\Bot\Api\Entities\Messages\Stickers\Sticker;
 
@@ -13,12 +14,13 @@ use Manuylenko\Telegram\Bot\Api\Entities\Messages\Stickers\Sticker;
  *
  * @link https://core.telegram.org/bots/api#gift
  *
- * @method   string getId()                   Уникальный идентификатор подарка.
- * @method  Sticker getSticker()              Объект стикера, символизирующий подарок.
- * @method      int getStarCount()            Количество Telegram Stars, которое необходимо заплатить для отправки стикера.
- * @method int|null getUpgradeStarCount() (+) Количество Telegram Stars, которое необходимо заплатить, чтобы сделать подарок уникальным.
- * @method int|null getTotalCount()       (+) Общее количество подарков данного типа, которое можно отправить; только для ограниченных подарков.
- * @method int|null getRemainingCount()   (+) Количество оставшихся подарков данного типа, которые можно отправить; только для ограниченных подарков.
+ * @method    string getId()                   Уникальный идентификатор подарка.
+ * @method   Sticker getSticker()              Объект стикера, символизирующий подарок.
+ * @method       int getStarCount()            Количество Telegram Stars, которое необходимо заплатить для отправки стикера.
+ * @method  int|null getUpgradeStarCount() (+) Количество Telegram Stars, которое необходимо заплатить, чтобы сделать подарок уникальным.
+ * @method  int|null getTotalCount()       (+) Общее количество подарков данного типа, которое можно отправить; только для ограниченных подарков.
+ * @method  int|null getRemainingCount()   (+) Количество оставшихся подарков данного типа, которые можно отправить; только для ограниченных подарков.
+ * @method Chat|null getPublisherChat()    (+) Информация о чате, опубликовавшем подарок.
  */
 #[Required([
     'id',
@@ -26,7 +28,8 @@ use Manuylenko\Telegram\Bot\Api\Entities\Messages\Stickers\Sticker;
     'star_count'
 ])]
 #[Depends([
-    'sticker' => Sticker::class
+    'sticker' => Sticker::class,
+    'publisher_chat' => Chat::class
 ])]
 class Gift extends Entity
 {
