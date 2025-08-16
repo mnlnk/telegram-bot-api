c8feae7c234d73bc4be4606b9d5d6b071b64dcaa
mnlnk <u.mnlnk@gmail.com>
2025-08-16 12:56:25 +0500

    SuggestedPostParameters: Добавляет методы для установки параметров и создания объекта сущности

--- src/Entities/Suggestions/SuggestedPostParameters.php
+++ src/Entities/Suggestions/SuggestedPostParameters.php
@@ -14,6 +14,9 @@ use Manuylenko\Telegram\Bot\Api\Entities\Entity;
  * @method SuggestedPostPrice|null getPrice()    (+) Предлагаемая цена за публикацию. Если поле не заполнено, публикация неоплачена.
  * @method                int|null getSendDate() (+) Предлагаемая дата отправки публикации. Если указано, то дата должна находиться в диапазоне от 300 до 2678400 секунд (30 дней). Если поле не заполнено, публикация может быть опубликована в любое время в течение 30 дней по усмотрению пользователя, одобрившего её.
  *
+ * @method $this setPrice(SuggestedPostPrice $price) Предлагаемая цена за публикацию. Если поле не заполнено, публикация неоплачена.
+ * @method $this setSendDate(int $sendDate)          Предлагаемая дата отправки публикации. Если указано, то дата должна находиться в диапазоне от 300 до 2678400 секунд (30 дней). Если поле не заполнено, публикация может быть опубликована в любое время в течение 30 дней по усмотрению пользователя, одобрившего её.
+ *
  * @since 9.2
  */
 #[Depends([
@@ -21,5 +24,14 @@ use Manuylenko\Telegram\Bot\Api\Entities\Entity;
 ])]
 class SuggestedPostParameters extends Entity
 {
-    //
+    /**
+     * Создает объект сущности.
+     */
+    public static function make(
+        SuggestedPostPrice $price = null,
+        int $sendDate = null
+    ): static
+    {
+        return static::fromArgs(func_get_args());
+    }
 }
