074fac2d34a8adcdc74ca57af76ddbfe44b06469
mnlnk <u.mnlnk@gmail.com>
2025-08-16 12:54:45 +0500

    SuggestedPostPrice: Добавляет методы для установки параметров и создания объекта сущности

--- src/Entities/Suggestions/SuggestedPostPrice.php
+++ src/Entities/Suggestions/SuggestedPostPrice.php
@@ -14,6 +14,9 @@ use Manuylenko\Telegram\Bot\Api\Entities\Entity;
  * @method string getCurrency() Валюта, в которой будет оплачена публикация. В настоящее время это может быть "XTR" для звезд Телеграм или "TON" для тонкоинов.
  * @method    int getAmount()   Сумма, которая будет выплачена за публикацию, в самых маленьких единицах. Для звезд Телеграм должна быть от 5 до 100 000, а цена в nano-toncoins — от 1 000 000 до 1 000 000 0000000.
  *
+ * @method $this setCurrency(string $currency) Валюта, в которой будет оплачена публикация. В настоящее время это может быть "XTR" для звезд Телеграм или "TON" для тонкоинов.
+ * @method $this setAmount(int $amount)        Сумма, которая будет выплачена за публикацию, в самых маленьких единицах. Для звезд Телеграм должна быть от 5 до 100 000, а цена в nano-toncoins — от 1 000 000 до 1 000 000 0000000.
+ *
  * @since 9.2
  */
 #[Required([
@@ -22,5 +25,14 @@ use Manuylenko\Telegram\Bot\Api\Entities\Entity;
 ])]
 class SuggestedPostPrice extends Entity
 {
-    //
+    /**
+     * Создает объект сущности.
+     */
+    public static function make(
+        string $currency,
+        int $amount
+    ): static
+    {
+        return static::fromArgs(func_get_args());
+    }
 }
