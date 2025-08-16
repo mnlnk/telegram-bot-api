a90b4bd836d8e8374e705978794090d54b868b70
mnlnk <u.mnlnk@gmail.com>
2025-08-16 12:58:14 +0500

    API: Добавляет параметр $suggestedPostParameters в метод sendPhoto()

--- src/Api.php
+++ src/Api.php
@@ -1167,6 +1167,7 @@ class Api
         ?bool $protectContent = null,
         ?bool $allowPaidBroadcast = null,
         ?string $messageEffectId = null,
+        ?SuggestedPostParameters $suggestedPostParameters = null,
         ?int $messageThreadId = null,
         ?int $directMessagesTopicId = null,
         ?ReplyParameters $replyParameters = null,
@@ -1176,6 +1177,7 @@ class Api
     {
         return EntityFactory::make(Message::class, $this->call(func_get_args(), [
             'caption_entities',
+            'suggested_post_parameters',
             'reply_parameters',
             'reply_markup'
         ]));
