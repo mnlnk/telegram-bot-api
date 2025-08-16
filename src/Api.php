ff63f180b21e22ae228d151254a5089c5c075b5c
mnlnk <u.mnlnk@gmail.com>
2025-08-16 13:03:29 +0500

    API: Добавляет параметр $suggestedPostParameters в метод sendVoice()

--- src/Api.php
+++ src/Api.php
@@ -1241,6 +1241,7 @@ class Api
         ?bool $protectContent = null,
         ?bool $allowPaidBroadcast = null,
         ?string $messageEffectId = null,
+        ?SuggestedPostParameters $suggestedPostParameters = null,
         ?int $messageThreadId = null,
         ?int $directMessagesTopicId = null,
         ?ReplyParameters $replyParameters = null,
@@ -1250,6 +1251,7 @@ class Api
     {
         return EntityFactory::make(Message::class, $this->call(func_get_args(), [
             'caption_entities',
+            'suggested_post_parameters',
             'reply_parameters',
             'reply_markup'
         ]));
