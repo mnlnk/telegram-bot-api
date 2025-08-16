8682fb0e3493be6d5449f9f0f492daa27aaa8a2b
mnlnk <u.mnlnk@gmail.com>
2025-08-16 13:10:03 +0500

    API: Добавляет параметр $suggestedPostParameters в метод sendVenue()

--- src/Api.php
+++ src/Api.php
@@ -1276,6 +1276,7 @@ class Api
         ?bool $protectContent = null,
         ?bool $allowPaidBroadcast = null,
         ?string $messageEffectId = null,
+        ?SuggestedPostParameters $suggestedPostParameters = null,
         ?int $messageThreadId = null,
         ?int $directMessagesTopicId = null,
         ?ReplyParameters $replyParameters = null,
@@ -1284,6 +1285,7 @@ class Api
     ): Message
     {
         return EntityFactory::make(Message::class, $this->call(func_get_args(), [
+            'suggested_post_parameters',
             'reply_parameters',
             'reply_markup'
         ]));
