cfb6ee3136ee3da2351abaf7513b32ae1f20be9f
mnlnk <u.mnlnk@gmail.com>
2025-08-16 12:09:13 +0500

    Добавляет класс сущности DirectMessagesTopic

---
+++ src/Entities/Messages/DirectMessagesTopic.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Manuylenko\Telegram\Bot\Api\Entities\Messages;
+
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Depends;
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
+use Manuylenko\Telegram\Bot\Api\Entities\Entity;
+use Manuylenko\Telegram\Bot\Api\Entities\User;
+
+/**
+ * Представляет тему личного чата.
+ *
+ * @link https://core.telegram.org/bots/api#directmessagestopic
+ *
+ * @method       int getTopicId()     Уникальный идентификатор темы.
+ * @method User|null getUser()    (+) Информация о пользователе, создавшем тему.
+ */
+#[Required([
+    'topic_id'
+])]
+#[Depends([
+    'user' => User::class
+])]
+class DirectMessagesTopic extends Entity
+{
+    //
+}
