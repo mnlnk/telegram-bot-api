14d49b29e543e9d0896ab74db2121487a817a7e2
mnlnk <u.mnlnk@gmail.com>
2026-01-01 13:04:36 +0500

    ChatFullInfo: Исправляет описания методов
    * getPhoto()
    * getBirthdate()
    * getBusinessIntro()
    * getBusinessLocation()
    * getBusinessOpeningHours()
    * getPersonalChat()
    * getParentChat()
    * getAvailableReactions()
    * getDescription()
    * getPinnedMessage()
    * getPermissions()
    * getAcceptedGiftTypes()
    * getLocation()

--- src/Entities/Chat/ChatFullInfo.php
+++ src/Entities/Chat/ChatFullInfo.php
@@ -29,15 +29,15 @@ use Manuylenko\Telegram\Bot\Api\Entities\Messages\Reaction\Types\ReactionType;
  * @method                 bool|null getIsDirectMessages()                   (+) Чат для личных сообщений канала.
  * @method                       int getAccentColorId()                          Идентификатор цвета акцента для имени чата и фона фотографии чата, заголовка ответа и предварительного просмотра ссылки.
  * @method                       int getMaxReactionCount()                       Максимальное количество реакций, которое можно установить на сообщение в чате.
- * @method            ChatPhoto|null getPhoto()                              (+) Объект фото чата.
+ * @method            ChatPhoto|null getPhoto()                              (+) Фото чата.
  * @method             string[]|null getActiveUsernames()                    (+) Список всех активных пользователей чата; для приватных чатов, супергрупп и каналов.
- * @method            Birthdate|null getBirthdate()                          (+) Объект даты рождения пользователя; для приватных чатов.
- * @method        BusinessIntro|null getBusinessIntro()                      (+) Объект бизнес-приветствия.
- * @method     BusinessLocation|null getBusinessLocation()                   (+) Объект местоположения бизнеса.
- * @method BusinessOpeningHours|null getBusinessOpeningHours()               (+) Объект часов работы бизнеса.
- * @method                 Chat|null getPersonalChat()                       (+) Объект чата личного канала пользователя; для приватных чатов.
- * @method                 Chat|null getParentChat()                         (+) Объект родительского чата; только для чатов личных сообщений.
- * @method       ReactionType[]|null getAvailableReactions()                 (+) Массив объектов доступных реакций, разрешенных в чате.
+ * @method            Birthdate|null getBirthdate()                          (+) Дата рождения пользователя; для приватных чатов.
+ * @method        BusinessIntro|null getBusinessIntro()                      (+) Бизнес-приветствие.
+ * @method     BusinessLocation|null getBusinessLocation()                   (+) Местоположения бизнеса.
+ * @method BusinessOpeningHours|null getBusinessOpeningHours()               (+) Часы работы бизнеса.
+ * @method                 Chat|null getPersonalChat()                       (+) Чат личного канала пользователя; для приватных чатов.
+ * @method                 Chat|null getParentChat()                         (+) Родительский чат; только для чатов личных сообщений.
+ * @method       ReactionType[]|null getAvailableReactions()                 (+) Доступные реакции, разрешенные в чате.
  * @method               string|null getBackgroundCustomEmojiId()            (+) Пользовательский идентификатор эмоджи, выбранного чатом для заголовка ответа и фона предварительного просмотра ссылки.
  * @method                  int|null getProfileAccentColorId()               (+) Идентификатор акцентного цвета фона профиля чата.
  * @method               string|null getProfileBackgroundCustomEmojiId()     (+) Пользовательский идентификатор эмоджи, выбранного чатом для фона своего профиля.
@@ -48,11 +48,11 @@ use Manuylenko\Telegram\Bot\Api\Entities\Messages\Reaction\Types\ReactionType;
  * @method                 bool|null getHasRestrictedVoiceAndVideoMessages() (+) Настройки приватности другой стороны запрещают отправку голосовых и видео-заметок в приватный чат.
  * @method                 bool|null getJoinToSendMessages()                 (+) Пользователям необходимо присоединиться к супергруппе, прежде чем они смогут отправлять сообщения.
  * @method                 bool|null getJoinByRequest()                      (+) Все пользователи, напрямую присоединяющиеся к супергруппе, должны быть одобрены администраторами супергруппы.
- * @method               string|null getDescription()                        (+) Описание; для групп, супергрупп и чатов каналов.
+ * @method               string|null getDescription()                        (+) Описание чата; для групп, супергрупп и чатов каналов.
  * @method               string|null getInviteLink()                         (+) Основная ссылка-приглашение; для групп, супергрупп и чатов каналов.
- * @method              Message|null getPinnedMessage()                      (+) Объект самого последнего закрепленного сообщения (по дате отправки).
- * @method      ChatPermissions|null getPermissions()                        (+) Объект разрешений (прав) участников чата по умолчанию для групп и супергрупп.
- * @method         AcceptedGiftTypes getAcceptedGiftTypes()                      Объект с информацией о типах подарков, которые принимаются чатом или пользователем для приватных чатов.
+ * @method              Message|null getPinnedMessage()                      (+) Последнее закрепленное сообщение (по дате отправки).
+ * @method      ChatPermissions|null getPermissions()                        (+) Разрешения (права) участников чата по умолчанию; для групп и супергрупп.
+ * @method         AcceptedGiftTypes getAcceptedGiftTypes()                      Типы подарков, которые принимаются чатом или пользователем; для приватных чатов.
  * @method                 bool|null getCanSendPaidMedia()                   (+) Разрешено отправлять или пересылать платные медиа-сообщения в чат канала.
  * @method                  int|null getSlowModeDelay()                      (+) Минимально допустимая задержка между последовательными сообщениями, отправляемыми каждым непривилегированным пользователем; в секундах.
  * @method                  int|null getUnrestrictBoostCount()               (+) Минимальное количество бустов, которое необходимо добавить пользователю, не являющемуся администратором, чтобы игнорировать медленный режим и разрешения чата; для супергрупп.
@@ -65,7 +65,7 @@ use Manuylenko\Telegram\Bot\Api\Entities\Messages\Reaction\Types\ReactionType;
  * @method                 bool|null getCanSetStickerSet()                   (+) Бот может менять набор стикеров группы.
  * @method               string|null getCustomEmojiStickerSetName()          (+) Название пользовательского набора эмоджи-стикеров группы; для супергрупп.
  * @method                  int|null getLinkedChatId()                       (+) Уникальный идентификатор связанного чата, т.е. идентификатор группы обсуждения для канала и наоборот; для супергрупп и чатов каналов.
- * @method         ChatLocation|null getLocation()                           (+) Объект местоположения, к которому привязана супергруппа.
+ * @method         ChatLocation|null getLocation()                           (+) Местоположение (локация), к которому привязана супергруппа.
  */
 #[Required([
     'id',
