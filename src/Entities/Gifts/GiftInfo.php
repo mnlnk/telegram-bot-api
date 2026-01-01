270a3989f3192e86efad30db41468488849925a5
mnlnk <u.mnlnk@gmail.com>
2026-01-01 11:55:28 +0500

    GiftInfo: Добавляет метод getIsUpgradeSeparate()
    + getIsUpgradeSeparate()

--- src/Entities/Gifts/GiftInfo.php
+++ src/Entities/Gifts/GiftInfo.php
@@ -17,6 +17,7 @@ use Manuylenko\Telegram\Bot\Api\Entities\Messages\MessageEntity;
  * @method          string|null getOwnedGiftId()             (+) Уникальный идентификатор полученного подарка для бота; присутствует только для подарков, полученных от имени бизнес-аккаунта.
  * @method             int|null getConvertStarCount()        (+) Количество звёзд Телеграм, которые получатель может получить, конвертировав подарок; не указывается, если конвертация в звёзд Телеграм невозможна.
  * @method             int|null getPrepaidUpgradeStarCount() (+) Количество звёзд Телеграм, которые были предоплачены отправителем за возможность улучшить подарок.
+ * @method            bool|null getIsUpgradeSeparate()       (+) Улучшение подарка было приобретено после отправки подарка.
  * @method            bool|null getCanBeUpgraded()           (+) Подарок можно сделать уникальным.
  * @method          string|null getText()                    (+) Текст сообщения, добавленного к подарку.
  * @method MessageEntity[]|null getEntities()                (+) Специальные сущности, которые появляются в тексте.
