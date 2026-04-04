f6aca6eb26c4457951eff455a120481ddfede6e9
mnlnk <u.mnlnk@gmail.com>
2026-04-04 15:59:51 +0500

    PollOption: Добавляет метод getAddedByChat()

--- src/Entities/Messages/Poll/PollOption.php
+++ src/Entities/Messages/Poll/PollOption.php
@@ -5,6 +5,7 @@ namespace Manuylenko\Telegram\Bot\Api\Entities\Messages\Poll;
 
 use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Depends;
 use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
+use Manuylenko\Telegram\Bot\Api\Entities\Chat\Chat;
 use Manuylenko\Telegram\Bot\Api\Entities\Entity;
 use Manuylenko\Telegram\Bot\Api\Entities\Messages\MessageEntity;
 use Manuylenko\Telegram\Bot\Api\Entities\User;
@@ -18,7 +19,8 @@ use Manuylenko\Telegram\Bot\Api\Entities\User;
  * @method               string getText()             Текст опции.
  * @method MessageEntity[]|null getTextEntities() (+) Массив специальных сущностей, которые появляются в тексте варианта (только пользовательские эмодзи).
  * @method                  int getVoterCount()       Количество пользователей, проголосовавших за этот вариант.
- * @method                 User getAddedByUser()  (+) Пользователь, добавивший опцию; если опция не была добавлена пользователем после создания опроса, она не указывается.
+ * @method            User|null getAddedByUser()  (+) Пользователь, добавивший опцию; если опция не была добавлена пользователем после создания опроса, она не указывается.
+ * @method            Chat|null getAddedByChat()  (+) Чат, добавивший эту опцию; опускается, если опция не была добавлена чатом после создания опроса.
  */
 #[Required([
     'persistent_id',
@@ -27,7 +29,8 @@ use Manuylenko\Telegram\Bot\Api\Entities\User;
 ])]
 #[Depends([
     'text_entities' => [MessageEntity::class],
-    'added_by_user' => User::class
+    'added_by_user' => User::class,
+    'added_by_chat' => Chat::class
 ])]
 class PollOption extends Entity
 {
