17fdf6927d7b1569232b4a746ac9bbccfd240706
mnlnk <u.mnlnk@gmail.com>
2026-04-04 16:19:03 +0500

    Добавляет класс сущности PollOptionAdded

---
+++ src/Entities/Messages/Poll/PollOptionAdded.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Manuylenko\Telegram\Bot\Api\Entities\Messages\Poll;
+
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Depends;
+use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
+use Manuylenko\Telegram\Bot\Api\Entities\Entity;
+use Manuylenko\Telegram\Bot\Api\Entities\Messages\MaybeInaccessibleMessage;
+use Manuylenko\Telegram\Bot\Api\Entities\Messages\MessageEntity;
+
+/**
+ * Представляет служебное сообщение о добавлении варианта ответа в опрос.
+ *
+ * @link https://core.telegram.org/bots/api#polloptionadded
+ *
+ * @method MaybeInaccessibleMessage|null getPollMessage()        (+) Сообщение, содержащее опрос, к которому был добавлен этот вариант ответа, если он известен.
+ * @method                        string getOptionPersistentId()     Уникальный идентификатор добавленной опции.
+ * @method                        string getOptionText()             Текст опции.
+ * @method          MessageEntity[]|null getOptionTextEntities() (+) Специальные сущности, которые отображаются в option_text.
+ */
+#[Required([
+    'option_persistent_id',
+    'option_text'
+])]
+#[Depends([
+    'poll_message' => MaybeInaccessibleMessage::class,
+    'option_text_entities' => [MessageEntity::class]
+])]
+class PollOptionAdded extends Entity
+{
+    //
+}
