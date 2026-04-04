535e8c979e9cca5abc8dae1fa244514840cc44d9
mnlnk <u.mnlnk@gmail.com>
2026-04-04 15:52:49 +0500

    PollOption: Добавляет метод getAddedByUser()

--- src/Entities/Messages/Poll/PollOption.php
+++ src/Entities/Messages/Poll/PollOption.php
@@ -7,6 +7,7 @@ use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Depends;
 use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
 use Manuylenko\Telegram\Bot\Api\Entities\Entity;
 use Manuylenko\Telegram\Bot\Api\Entities\Messages\MessageEntity;
+use Manuylenko\Telegram\Bot\Api\Entities\User;
 
 /**
  * Представляет информацию об одном варианте ответа в опросе.
@@ -17,6 +18,7 @@ use Manuylenko\Telegram\Bot\Api\Entities\Messages\MessageEntity;
  * @method               string getText()             Текст опции.
  * @method MessageEntity[]|null getTextEntities() (+) Массив специальных сущностей, которые появляются в тексте варианта (только пользовательские эмодзи).
  * @method                  int getVoterCount()       Количество пользователей, проголосовавших за этот вариант.
+ * @method                 User getAddedByUser()  (+) Пользователь, добавивший опцию; если опция не была добавлена пользователем после создания опроса, она не указывается.
  */
 #[Required([
     'persistent_id',
@@ -24,7 +26,8 @@ use Manuylenko\Telegram\Bot\Api\Entities\Messages\MessageEntity;
     'voter_count'
 ])]
 #[Depends([
-    'text_entities' => [MessageEntity::class]
+    'text_entities' => [MessageEntity::class],
+    'added_by_user' => User::class
 ])]
 class PollOption extends Entity
 {
