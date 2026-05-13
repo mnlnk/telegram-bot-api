0a39ff3e6b9c9978bdc267fb43a9e7bc1c4c8f02
mnlnk <u.mnlnk@gmail.com>
2026-05-14 00:58:44 +0500

    InputPaidMediaPhoto: Исправляет описание

--- src/Entities/Paid/Input/InputPaidMediaPhoto.php
+++ src/Entities/Paid/Input/InputPaidMediaPhoto.php
@@ -7,7 +7,7 @@ use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Required;
 use Manuylenko\Telegram\Bot\Api\Entities\Paid\PaidMediaType;
 
 /**
- * Представляет платные медиа (фото).
+ * Представляет платное медиа (фото).
  *
  * @link https://core.telegram.org/bots/api#inputpaidmediaphoto
  *
