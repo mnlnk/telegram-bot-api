cf3e5b3596042f74865fb581948b06a3b444e1f9
mnlnk <u.mnlnk@gmail.com>
2026-05-14 00:45:37 +0500

    InputPaidMedia: Исправляет реализацию метода getConcrete()

--- src/Entities/Paid/Input/InputPaidMedia.php
+++ src/Entities/Paid/Input/InputPaidMedia.php
@@ -5,7 +5,7 @@ namespace Manuylenko\Telegram\Bot\Api\Entities\Paid\Input;
 
 use Manuylenko\Telegram\Bot\Api\Entities\Attributes\Concrete;
 use Manuylenko\Telegram\Bot\Api\Entities\Entity;
-use Manuylenko\Telegram\Bot\Api\Entities\Input\InputType;
+use Manuylenko\Telegram\Bot\Api\Entities\Paid\PaidType;
 
 /**
  * Представляет платные медиа, добавленные в сообщение.
@@ -24,8 +24,8 @@ abstract class InputPaidMedia extends Entity
     public static function getConcrete(array $data): ?static
     {
         return match ($data['type']) {
-            InputType::PHOTO => new InputPaidMediaPhoto($data),
-            InputType::VIDEO => new InputPaidMediaVideo($data),
+            PaidType::PHOTO => new InputPaidMediaPhoto($data),
+            PaidType::VIDEO => new InputPaidMediaVideo($data),
             default               => null
         };
     }
